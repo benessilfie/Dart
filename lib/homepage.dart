0fe8280106decf0b490475e5e22af0b9d92b3249
Benjamin Ofori-Quansah <bquansah007@gmail.com>
2022-01-05 23:42:11 +0000

    commit to master

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -60,7 +60,8 @@ class HomePage extends StatelessWidget {
              title: Text('Micheal Nyarko', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),),
              subtitle: Text('+233 56 342 8965'),
              trailing: IconButton(onPressed: null, icon: Icon(Icons.more_horiz)),
-           );}, 
+           );
+        }, 
 
           separatorBuilder: (context, index){
             return const Divider(
@@ -74,14 +75,32 @@ class HomePage extends StatelessWidget {
          
          GroupedListView<dynamic, String>(
            shrinkWrap: true,
+
     elements: const [
       {'name':'Abena', 'phone_number':'+233 56 958 7251'},
       {'name':'Abena', 'phone_number':'+233 56 958 7251'},
       {'name':'Abena', 'phone_number':'+233 56 958 7251'}],
+
     groupBy: (element) => element['name'].toString().substring(0,1),
-    groupSeparatorBuilder: (String groupByValue) => Text(groupByValue.substring(0,1)),
-    itemBuilder: (context, dynamic element) => Text(element['name']),
-    itemComparator: (item1, item2) => item1['name'].compareTo(item2['name']), // optional
+    groupSeparatorBuilder: (String groupByValue) => 
+    SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: Text(groupByValue.substring(0,1), textAlign: TextAlign.right, 
+      style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 18),),
+    ),
+    
+    itemBuilder: (context, dynamic element) => const ListTile(
+             leading: CircleAvatar(
+             radius: 22,
+             backgroundImage: AssetImage('assets/adom.jpg'),),
+             title: Text('Micheal Nyarko', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),),
+             subtitle: Text('+233 56 342 8965'),
+             trailing: IconButton(onPressed: null, icon: Icon(Icons.more_horiz)),
+           ),
+
+    itemComparator: (item1, item2) => 
+    item1['name'].compareTo(item2['name']), // optional
+
     useStickyGroupSeparators: true, // optional
     floatingHeader: true, // optional
     order: GroupedListOrder.ASC, // optional
