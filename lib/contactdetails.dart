76bf4f2dc9d0526e2f213da3451464182b063cdf
Benjamin Ofori-Quansah <bquansah007@gmail.com>
2022-01-06 09:02:07 +0000

    commit to master

--- lib/contactdetails.dart
+++ lib/contactdetails.dart
@@ -20,7 +20,47 @@ class ContactDetailsPage extends StatelessWidget {
         style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600,
         color: Colors.black),),
 
-        
+        actions: [
+          IconButton(onPressed: (){}, icon: const Icon(Icons.more_vert),),
+        ]
+      ),
+
+      body: ListView(
+        children: [
+        const  CircleAvatar(
+            radius: 70,
+            backgroundImage: AssetImage('assets/abena.jpg'),
+          ),
+
+         const SizedBox(height: 25,),
+         const Center(
+            child: Text('Full name',
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600,),),
+          ),
+
+        const  Center(child: Text('Location', style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal,),)),
+        const  SizedBox(height: 30,),
+
+          Container(
+            color: const Color(0xff9AADBE),
+            child: Column(
+              children: [
+                ListTile (
+                  title: const Text('Mobile',style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600,),),
+                  subtitle: const Text('+233 25 256 8547', 
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal, color: Color(0xff333333)),),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextButton(onPressed: (){}, child: const Icon(Icons.message, color: Colors.black,),),
+                      const IconButton(onPressed: null, icon: Icon(Icons.call),),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
